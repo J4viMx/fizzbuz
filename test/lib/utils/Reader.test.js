8d8ab534d45ee2ff32becd88fa4607ff715d0c5c
JaviMx <javimxmine@hotmail.com>
2022-05-01 17:14:56 -0500

    Prueba de reader creada

---
+++ test/lib/utils/Reader.test.js
@@ -0,0 +1,9 @@
+const Reader = require( "../../../lib/utils/Reader.js" )
+
+describe("Puebas a Reader", () => {
+
+    test('Leer el archivo Json', () => { 
+        const explorers = Reader.readJsonFile("explorers.json")
+        expect(explorers.length).toBe(15)
+     })
+});
